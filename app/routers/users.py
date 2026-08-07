93c0c615321d7125b0b22c37be067981582b8d4c
lebowsskii <neepooha.1991@gmail.com>
2026-08-07 17:51:59 +0500

    rm respone

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,10 +1,9 @@
 import uuid
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter
 
 from app.crud import users as users_crud
 from app.dependencies import DBSessionDep
-from app.schemas.error import ErrorResponse
 from app.schemas.user import User
 from app.utils.exceptions import NotFoundError
 
@@ -16,10 +15,7 @@ async def list_users(session: DBSessionDep) -> list[User]:
     return await users_crud.get_all(session)
 
 
-@router.get(
-    "/{user_id}",
-    responses={status.HTTP_404_NOT_FOUND: {"model": ErrorResponse}},
-)
+@router.get("/{user_id}")
 async def get_user(user_id: uuid.UUID, session: DBSessionDep) -> User:
     user = await users_crud.get_by_id(session, user_id)
     if not user:
