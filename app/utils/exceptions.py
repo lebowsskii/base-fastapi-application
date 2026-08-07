f6047726812f6c04edff0bf61b1c89accf204710
lebowsskii <neepooha.1991@gmail.com>
2026-08-07 17:52:28 +0500

    lint project

--- app/utils/exceptions.py
+++ app/utils/exceptions.py
@@ -68,9 +68,7 @@ def register_exception_handlers(app: FastAPI) -> None:
     async def app_error_handler(_: Request, exc: AppError) -> JSONResponse:
         return JSONResponse(
             status_code=exc.status_code,
-            content=jsonable_encoder(
-                ErrorResponse(code=exc.code, message=exc.message)
-            ),
+            content=jsonable_encoder(ErrorResponse(code=exc.code, message=exc.message)),
         )
 
     @app.exception_handler(RequestValidationError)
